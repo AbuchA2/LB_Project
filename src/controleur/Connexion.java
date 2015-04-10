69912686c62695b2472ddb5fbef6e8af089e67f4
Aude Buch <aude.buchmuller@free.fr>
2015-04-10 11:16:57 +0200

    servlet Connexion.java modifiée

--- src/controleur/Connexion.java
+++ src/controleur/Connexion.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class Connexion
  */
 @WebServlet(name = "connexion", urlPatterns = { "/connexion" })
-
+        
 public class Connexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
