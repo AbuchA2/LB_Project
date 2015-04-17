375e53a4b76c1f083c7ba0593a880d5de31aac00
AbuchA2 <aude.buchmuller@isep.fr>
2015-04-17 15:27:39 +0200

    Modification de Connexion.java

--- src/controleur/Connexion.java
+++ src/controleur/Connexion.java
@@ -15,11 +15,14 @@ import javax.servlet.http.HttpServletResponse;
 public class Connexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-    public static final String VUE = "/WEB-INF/inscription.jsp";
-    public static final String CHAMP_EMAIL = "email";
-    public static final String CHAMP_PASS = "motdepasse";
-    public static final String CHAMP_CONF = "confirmation";
+    public static final String VUE = "/WEB-INF/connexion.jsp";
+    //public static final String VUE = "/WEB-INF/inscription.jsp";
     public static final String CHAMP_NOM = "nom";
+    public static final String CHAMP_PASS = "motdepasse";
+    public static final String CHAMP_SOUV = "souvenir";
+//    public static final String CHAMP_EMAIL = "email";
+//    public static final String CHAMP_CONF = "confirmation";
+
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -35,23 +38,30 @@ public class Connexion extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-        String email = request.getParameter( CHAMP_EMAIL );
-        String motDePasse = request.getParameter( CHAMP_PASS );
-        String confirmation = request.getParameter( CHAMP_CONF );
+
         String nom = request.getParameter( CHAMP_NOM );
+        String motDePasse = request.getParameter( CHAMP_PASS );
+        String souvenir = request.getParameter( CHAMP_SOUV );
+        //String email = request.getParameter( CHAMP_EMAIL );
+        //String confirmation = request.getParameter( CHAMP_CONF );
+
 
         try {
-            validationEmail( email );
-            validationMotsDePasse( motDePasse, confirmation );
+            //validationEmail( email );
+            //validationMotsDePasse( motDePasse, confirmation );
             validationNom( nom );
+            validationMotsDePasse( motDePasse );
+            validationSouv( souvenir);
+            
         } catch (Exception e) {
             /* Gérer les erreurs de validation ici. */
         }
     }
 
-    private void validationEmail( String email ) throws Exception{}
-    private void validationMotsDePasse( String motDePasse, String confirmation ) throws Exception{}
-    private void validationNom( String nom ) throws Exception{}
+    //private void validationEmail( String email ) throws Exception{}
+    //private void validationMotsDePasse( String motDePasse, String confirmation ) throws Exception{}
+    private void validationMotsDePasse( String motDePasse) throws Exception{}
+	private void validationNom( String nom ) throws Exception{}
+	private void validationSouv( String souvenir ) throws Exception{}
 		
 }
